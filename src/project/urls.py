1e743777efd50373b4cfb74f2dd722b6ab7f3b42
Alexandr <alexandr.tarltsky@gmail.com>
2020-11-24 16:45:56 +0300

    format

--- src/project/urls.py
+++ src/project/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import path
 
 
 def index(request: HttpRequest) -> HttpResponse:
